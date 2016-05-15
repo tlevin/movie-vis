4bb069f430a55004e5e773ec1c7e85e0bc417ed1
tlevin <toddlevin802@gmail.com>
2016-05-15 02:08:44 -0700

    Production bugfixing

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var Express = require('express');
 
 var app = Express();
 
-app.use(Express.static('dist'));
+app.use(Express.static(__dirname + '/dist'));
 
-var server = app.listen(80, function() {
+var server = app.listen(3000, function() {
   console.log('Express server running.')
 })
